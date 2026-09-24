e809c91d4fbba92fb514ce0af20d7e73327b7525
Vikas Srivastava <er.vikassri@gmail.com>
2026-09-24 11:59:59 +0800

    Fix start_date format in email_failure_test.py

--- email_failure_test.py
+++ email_failure_test.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 # Airflow automatically reads these arguments to trigger built-in SMTP alerts
 default_args = {
     "owner": "airflow",
-    "start_date": datetime.datetime(2026, 09, 25),
+    "start_date": datetime.datetime(2026, 9, 25),
     "email": ["alert_team@cldr.com", "admin@cldr.com"], 
     "email_on_failure": True,   # Send email if a task fails
     "email_on_retry": False,    # Do not send email on every retry attempt
